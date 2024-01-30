04014b52633a52e59f326b0ce3ec568fd0843667
LiquidEnder <79119607+LiquidEnder@users.noreply.github.com>
2024-01-29 21:54:05 -0600

    asteroid destruction now implemented fully.
    Asteroid destruction now fully implemented.

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -9,3 +9,9 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_area_entered(area):
+	if area is Asteroid:
+		var aster = area
+		aster.explode()
+		queue_free()
