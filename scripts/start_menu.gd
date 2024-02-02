16cf09f8b2037fe97cde3dfb4130e38b77328847
czajkatony <4tonyc4@gmail.com>
2024-02-01 20:16:36 -0600

    Added design title screen

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -3,15 +3,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_process_input(true)
 
+func _input(event):
+	if event.is_action_pressed("shoot"):
+		start_game()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_quit_button_pressed():
-	get_tree().quit()
-
-func _on_start_button_pressed():
+func start_game():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
